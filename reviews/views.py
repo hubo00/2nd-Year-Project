4b558508f09d7f81eef76f0a7680e3406ebf2ccf
Hubert Bukowski <hub.bukowski@gmail.com>
2021-03-12 11:32:52 +0000

    Fixed review image upload

--- reviews/views.py
+++ reviews/views.py
@@ -16,12 +16,14 @@ def add_review(request, prod_slug):
             rating = form.cleaned_data['rating']
             content = form.cleaned_data['content']
             username = form.cleaned_data['username']
+            image = form.cleaned_data['image']
             review = Review()
             review.product = product
             review.username = username
             review.rating = rating
             review.content = content
             review.pub_date = datetime.datetime.now()
+            review.image = image
             review.save()
 
             return HttpResponseRedirect(reverse('shop:prod_detail', kwargs={'prod_slug': prod_slug}))
